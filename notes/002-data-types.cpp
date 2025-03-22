1a676cf22abb34842adfb10485266547f211ab72
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-22 15:06:54 -0300

    add 002-data-types.cpp and refactor file names

---
+++ notes/002-data-types.cpp
@@ -0,0 +1,27 @@
+/*
+    Most used data types
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    // Integer number
+    int n1 = 3;
+
+    // Double: floating-point number
+    double n2 = 3.1;
+
+    // Char: individual character. Requires single-quotes ('')
+    char m1 = 'a';
+
+    // String: sequence of characters. Requires <string> header
+    std::string openSecret = "Bitcoin";
+
+    // Boolean: true/false
+    bool isRevolution = true;
+
+    return 0;
+
+}
