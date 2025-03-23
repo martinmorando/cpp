56e096529af2728c0ac8e124cbadc5b1bb6a1dd4
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 10:12:14 -0300

    add 003-errors.cpp and refactor file names

---
+++ notes/003-errors.cpp
@@ -0,0 +1,19 @@
+/*
+    Errors
+
+    A) Compile Errors: detected by the compiler
+        A1) Syntax errors: the code violates the C++ synxtax rules
+            - Missing semicolon
+            - Unmatched parenthesis 
+        A2) Type errors: 
+            - Forget variable declaration
+            - Store value of a wrong type
+
+    B) Link-time errors: detected by the linker. Examples:
+        - Missing library files
+        - Missing functions
+
+    C) Run-time errors: occur during program execution (no compile errors, no link-time errors)
+
+    D) Logical errors: flawed logic by programmer/s
+*/
