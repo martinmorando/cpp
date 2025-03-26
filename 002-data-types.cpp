e0cdeedd8c4840cfbda88963fc511d25239c46ca
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-26 20:29:09 -0300

    add constant and type casting in 002-data-types.cpp

--- 002-data-types.cpp
+++ 002-data-types.cpp
@@ -1,5 +1,5 @@
 /*
-    Most used data types
+    Some data types
 */
 
 #include <iostream>
@@ -7,20 +7,34 @@
 
 int main() {
 
-    // Integer number
+    // [DATA TYPES]
+    // - Integer number
     int n1 = 3;
 
-    // Double: floating-point number
+    // - Double: floating-point number
     double n2 = 3.1;
 
-    // Char: individual character. Requires single-quotes ('')
+    // - Char: individual character. Requires single-quotes ('')
     char m1 = 'a';
 
-    // String: sequence of characters. Requires <string> header
+    // - String: sequence of characters. Requires <string> header
     std::string openSecret = "Bitcoin";
 
-    // Boolean: true/false
+    // - Boolean: true/false
     bool isRevolution = true;
+
+
+
+    // [CONSTANT]
+    const int iCantChange = 10;
+    // iCantChange = 11;    // error: assignment of read-only variable ‘iCantChange’
+
+
+    // [TYPE CASTING]: convert from one type to another    
+    double n3 = 2.11111;
+    int n4 = (int) n3;      // Double to integer
+    std::cout << n4;        // Output: 2
+
 
     return 0;
 
